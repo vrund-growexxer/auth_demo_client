7c434a55224ad191b76ce71c1e7cc9c920c5b50b
Vrund Shah <vrund.shah@growexx.com>
2023-03-07 18:31:16 +0530

    Add post UI

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -3,6 +3,7 @@ import { Box } from "@mui/system";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -24,6 +25,7 @@ const HomePage = () => {
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
+          <MyPostWidget picturePath={picturePath}/>
         </Box>
         {isNonMobileScreens && (
           <Box flexBasis="26%">
